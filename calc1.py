c263b4c3d68a43e0a802afbec77a18ad3c8e5078
Shubham <tshubhamthakur@gmail.com>
2019-01-23 12:31:46 +0530

    continue function

--- calc1.py
+++ calc1.py
@@ -1,29 +1,62 @@
-def add(c):
-    c = c + a
-    return c
+def add(x, y):
+   return x + y
 
-def sub(c):
-    c = c + a
-    return c
+# This function subtracts two numbers
+def subtract(x, y):
+   return x - y
 
-def mul(c):
-    c = c + a
-    return c
+# This function multiplies two numbers
+def multiply(x, y):
+   return x * y
 
-def div(c):
-    c = c + a
-    return c
-
+# This function divides two numbers
+def divide(x, y):
+   return x / y
+   
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
-choice = input("Enter choice(1/2/3/4/5):")
-if choice == '1':
-    a = int(input("enter number: "))
-    x = []
-    n = 10
-    for
-    print(add(a))
-    print(add(a))
+print("Press 5 for exit: ")
+
+
+
+# Continuous Menu -> 0
+# Invalid number
+# Red on error
+# Unlimited numbers -> Empty value
+
+# Take input from the user
+while True:
+
+    choice = input("Enter choice(1/2/3/4/5): ")
+
+
+
+
+    if choice == '1':
+            num1 = int(input("Enter first number: "))
+            num2 = int(input("Enter second number: "))
+            print(num1,"+",num2,"=", add(num1,num2))
+
+    elif choice == '2':
+            num1 = int(input("Enter first number: "))
+            num2 = int(input("Enter second number: "))
+            print(num1,"-",num2,"=", subtract(num1,num2))
+
+    elif choice == '3':
+            num1 = int(input("Enter first number: "))
+            num2 = int(input("Enter second number: "))
+            print(num1,"*",num2,"=", multiply(num1,num2))
+
+    elif choice == '4':
+            num1 = int(input("Enter first number: "))
+            num2 = int(input("Enter second number: "))
+            print(num1,"/",num2,"=", divide(num1,num2))
+
+    elif choice == '5':
+        break
+        exit()
+
+print("thank you")
